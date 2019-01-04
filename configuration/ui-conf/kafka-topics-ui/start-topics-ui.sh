83cec83f4f66e8e049dfd3a93ce6ea13fdced558
Philippe Lawson <philippe.lawson@lawson-it.com>
2019-01-04 08:53:13 +0100

    Switched to daemon mode

--- configuration/ui-conf/kafka-topics-ui/start-topics-ui.sh
+++ configuration/ui-conf/kafka-topics-ui/start-topics-ui.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-docker run --rm -it -p 8001:8000 \
+#docker run --rm -it -p 8001:8000 \
+docker run -d -p 8001:8000 \
                -e "KAFKA_REST_PROXY_URL=http://192.168.1.59:8085" \
                -e "PROXY=true" \
                landoop/kafka-topics-ui
--- configuration/ui-conf/schema-registry-ui/start-schema-registry-ui.sh
+++ configuration/ui-conf/schema-registry-ui/start-schema-registry-ui.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-docker run --rm -p 8002:8000 \
+#docker run --rm -p 8002:8000 \
+docker run -d -p 8002:8000 \
            -e "SCHEMAREGISTRY_URL=http://192.168.1.59:8081" \
            landoop/schema-registry-ui
